572ea4edead1fd7e5744766d1d015d7a44e2794c
Ravele Silva <ravele.cs@gmail.com>
2023-10-03 11:25:16 -0300

    Projeto praticamente concluído
    Faltando apenas a feature de dados sobre a lista para o usuário

--- script.js
+++ script.js
@@ -2,24 +2,42 @@ const titulo = document.querySelector('.create input');
 const createButton = document.querySelector('.create .button');
 const lista = document.querySelector('.list');
 
+function deleteItem(item) {
+  item.remove();
+}
+
 function createItem() {
   if (titulo.value.length > 0) {
     const item = document.createElement('li');
     const input = document.createElement('input');
     const label = document.createElement('label');
+    const del = document.createElement('span');
+    const checkboxId = Date.now();
   
     input.setAttribute('name', 'item');
     input.setAttribute('type', 'checkbox');
-  
-    label.setAttribute('for', 'item');
+    input.setAttribute('id', checkboxId)
+
+    label.setAttribute('for', input.id);
     label.innerHTML = titulo.value;
-  
+    titulo.value = ''
+
+    del.addEventListener('click', () => deleteItem(item));
+
     item.appendChild(input);
     item.appendChild(label);
-  
+    item.appendChild(del);
+
     lista.appendChild(item);
+
   }
-  
 }
 
 createButton.addEventListener('click', createItem);
+window.addEventListener('keydown', function(event) {
+  const key = event.key;
+
+  if (key === 'Enter') {
+    createItem();
+  }
+})
